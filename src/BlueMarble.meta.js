59f224d696dbde0d19abf78f65a45ed65b562f50
Agatem <agaatem@outlook.com>
2025-08-15 12:55:07 +0200

    Remove `@run-at` (#161)
    * Added ignore files to stop popular code formatters from messing with the code style
    
    * Change transform easing to 0s
    
    * Added a translucent gray checkerboard render for #deface
    
    * Added workflow to check what branch PR came from
    
    * v0.81.0; Merge branch 'code' into main
    
    * Emergency patch...
    
    ...please stop informing me that the install link is broken 😭
    
    * v0.82.0; Emergency patch...
    
    ...please stop informing me that the install link is broken 😭
    
    * Remove `@run-at`
    
    ---------
    
    Co-authored-by: SwingTheVine <swingthevine@gmail.com>
    Co-authored-by: AloeSapling <aloesapling@gmail.com>
    Co-authored-by: Endrik Tombak <littleendu@gmail.com>
    Co-authored-by: KrunchyKrisp <m.damidavicius@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/BlueMarble.meta.js
+++ src/BlueMarble.meta.js
@@ -10,7 +10,6 @@
 // @icon         https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/f3ee47c55505d29255b29e320891453884f13369/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
