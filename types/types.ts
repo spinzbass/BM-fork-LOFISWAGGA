b43883c912c285f74951e03feaf4b6fd934ffdfc
AloeSapling <aloesapling@gmail.com>
2025-08-19 20:25:02 +0200

    Added some html

--- types/types.ts
+++ types/types.ts
@@ -1,12 +1,4 @@
 import { TBlueMarbleJSON, TCharityJSON } from "./schemas";
 
-// The Context objects contains data to be passed into the inject function that can only be accessed outside of it
-export type Context = {
-    HTMLData?: string;
-    CSSUrl?: string;
-}
-
-
-
 export type Schemas = TCharityJSON | TBlueMarbleJSON;
 export type SchemaTypeNames = "CHA" | "BM" | "N/A"; // Charity, BlueMarble or unknown
