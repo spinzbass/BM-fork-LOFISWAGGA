ad4aea7de99b741f8c422ff6f83f7a7540afa983
AloeSapling <aloesapling@gmail.com>
2025-08-20 22:57:47 +0200

    Build

--- src/createTemplate.ts
+++ src/createTemplate.ts
@@ -69,7 +69,6 @@ function createTemplate(){
 export function initCreateTemplate(){
     // Add event listener hooks
     const coordsBtn = document.querySelector("#bm-create-template button#coords");
-    window.charity.game.map.on("click", (e)=>{console.log(e)})
     console.log("coordsBtn: testestsetestseesseetestest ",coordsBtn)
     console.log(coordsBtn?.nodeName)
     console.log(coordsBtn?.nodeName.toLocaleUpperCase() === "BUTTON")
