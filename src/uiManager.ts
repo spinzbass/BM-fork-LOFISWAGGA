4ce132a652c20fb6954fda55014f12557efb33ec
AloeSapling <aloesapling@gmail.com>
2025-08-28 14:34:28 +0200

    added a safeguard against opening more than one window

--- src/uiManager.ts
+++ src/uiManager.ts
@@ -15,7 +15,7 @@ const DISPLAY_DEFAULTS: Record<WindowNames, string> = {
  */
 export default class UIManager{
 
-    /** Functions that update the UI of a given window (reinsert data, recreate table rows, ect) 
+    /**Functions that update the UI of a given window (reinsert data, recreate table rows, ect) 
      * @since 0.1.0-overhaul
      */
     updateFunctions: Record<WindowNames, Function | undefined> = {
@@ -25,20 +25,37 @@ export default class UIManager{
         "bm-manage-templates": undefined
     }
 
-    /**Hides the "window" with the corresponding id
+    /**The open state of a specific type of window (open = true, closed = false)
+     * @since 0.4.0-overhaul
+    */
+    windowOpen: Record<WindowNames, boolean> = {
+        "bm-create-template": false,
+        "bm-main-overlay": false,
+        "bm-manage-links": false,
+        "bm-manage-templates": false   
+    }
+
+    /**Hides the "window" with the corresponding id. Updates the open state of the "window" with the corresponding id
      * @param {WindowNames} windowName name / id of the window to hide
      * @since 0.1.0-overhaul
+     * @version 2.0
      */
     close(windowName : WindowNames){
 
+        this.windowOpen[windowName] = false;
         const bmWindow = document.querySelector("#"+windowName);
         if(bmWindow){ (bmWindow as HTMLElement).style.display = "none" }
     }
-    /**Displays the "window" with the corresponding id
+    /**Displays the "window" with the corresponding id. Updates the count of open "windows" with the corresponding id
      * @param {WindowNames} windowName name / id of the window to display
      * @since 0.1.0-overhaul
+     * @version 2.0
      */
     open(windowName: WindowNames){
+
+        // Don't open this window if a window of this type is already open
+        if(this.windowOpen[windowName]){ return };
+        this.windowOpen[windowName] = true;
 
         const bmWindow = document.querySelector("#"+windowName);
         if(bmWindow){
